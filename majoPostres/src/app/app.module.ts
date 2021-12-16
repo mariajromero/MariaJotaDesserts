4804f8e378c71c14b6175a7dd537c61eb4c59ce5
Maria Jose Romero <mariajromerov@outlook.com>
2021-12-16 11:22:54 -0500

    hacer pedido component

--- majoPostres/src/app/app.module.ts
+++ majoPostres/src/app/app.module.ts
@@ -5,6 +5,10 @@ import { MatToolbarModule } from '@angular/material/toolbar';
 import { FlexLayoutModule } from '@angular/flex-layout';
 import { MatListModule } from '@angular/material/list';
 import { MatButtonModule } from '@angular/material/button';
+import {MatRadioModule} from '@angular/material/radio';
+import { MatSelectModule } from '@angular/material/select';
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+import { ReactiveFormsModule } from '@angular/forms';
 
 import 'hammerjs';
 
@@ -12,17 +16,26 @@ import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { FooterComponent } from './footer/footer.component';
 import { HeaderComponent } from './header/header.component';
-
+import { VerMasComponent } from './ver-mas/ver-mas.component';
+import { HacerPedidoComponent } from './hacer-pedido/hacer-pedido.component';
+import {AppRoutingModule} from './app-routing/app-routing.module';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
     FooterComponent,
-    HeaderComponent
+    HeaderComponent,
+    VerMasComponent,
+    HacerPedidoComponent
   ],
   imports: [
+    MatSelectModule,
+    MatSlideToggleModule,
+    ReactiveFormsModule,
+    MatRadioModule,
     BrowserModule,
+    AppRoutingModule,
     BrowserAnimationsModule,
     MatToolbarModule,
     FlexLayoutModule,
---
+++ majoPostres/src/app/hacer-pedido/hacer-pedido.component.ts
@@ -0,0 +1,44 @@
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+
+import { Pedido, Otro } from '../shared/pedido';
+
+@Component({
+  selector: 'app-hacer-pedido',
+  templateUrl: './hacer-pedido.component.html',
+  styleUrls: ['./hacer-pedido.component.scss']
+})
+export class HacerPedidoComponent implements OnInit {
+  pedidoForm: FormGroup;
+  pedido: Pedido;
+  otro = Otro;
+
+  constructor(private fb: FormBuilder) {
+    this.createForm();
+  }
+
+  createForm() {
+    this.pedidoForm = this.fb.group({
+
+      email: '',
+    comentarios: '',
+    //imagen: '',
+    tipoPostre: '',
+    porciones:0,
+    //masaColor: '',
+    //masaSabor: '',
+    //coberturaColor: '',
+    //coberturaSabor: '',
+    otro:'None'
+  
+    });
+  }
+  onSubmit() {
+    this.pedido = this.pedidoForm.value;
+    console.log(this.pedido);
+    
+  }
+  ngOnInit() {
+  }
+
+}
