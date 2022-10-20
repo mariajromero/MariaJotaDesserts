d11d506ee2e535873e725d86b6a9eaaea8607496
mariajromero <mariajromerov@outlook.com>
2022-10-20 15:23:03 -0500

    cambios en app component ts

--- majoPostres/src/app/app.component.ts
+++ majoPostres/src/app/app.component.ts
@@ -1,10 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Postre } from './shared/postre';
+import { PostreService } from './shared/postre.service';
+import { HttpErrorResponse } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent  implements OnInit{
+  public postres:Postre[];
+  constructor(private postreService: PostreService){}
+  ngOnInit()  {
+    this.getPostres();
+  }
+
+  public getPostres():void{
+    this.postreService.getPostres().subscribe(
+      (response:Postre[])=>{
+        this.postres=response;
+      },
+      (error:HttpErrorResponse)=>{
+        alert(error.message);
+      }
+      
+    );
+  }
+
   title = 'majoPostres';
 }
--- majoPostres/src/app/shared/postre.ts
+++ majoPostres/src/app/shared/postre.ts
@@ -1,6 +1,6 @@
 export class Postre{
     name:string;
-    id:string;
+    id:number;
     image:string;
     category:string;
     description:string;
